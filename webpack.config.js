91f9aa463c0434e4e5aa60406f5f9240f13cf429
Benjamin Reid <benjamin@nouveller.com>
2017-05-15 13:04:32 +0100

    Fix webpack now we're using sagas and babel heavily

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,24 @@
 var path = require("path");
 
 module.exports = {
-  entry: "./demo/main.js",
+  entry: ["babel-polyfill", "./demo/main.js"],
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "demo"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+          },
+        },
+      },
+    ],
   },
   devServer: {
     contentBase: path.join(__dirname, "demo"),
